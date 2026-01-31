00bf3d1ee6a55a8a8133e11f309f8b654356acd1
crothen <crothen@users.noreply.github.com>
2026-01-31 22:34:45 +0100

    add firebase config (Firestore europe-west6)

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Firebase configuration for Gruppe Offene Synode
+export const firebaseConfig = {
+  apiKey: "AIzaSyDdwhb53zDuh18aOJKA0j5y-_Xx1SPIyrs",
+  authDomain: "gruppe-offene-synode.firebaseapp.com",
+  projectId: "gruppe-offene-synode",
+  storageBucket: "gruppe-offene-synode.firebasestorage.app",
+  messagingSenderId: "283371158939",
+  appId: "1:283371158939:web:9820dcc989a77b336f753e"
+};
